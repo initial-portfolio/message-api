4b41014fef1e8f6c8672957a22f51679444926d9
Arthur Bernardo <arthurutschgg@gmail.com>
2022-10-04 08:08:46 -0300

    web configurer

--- src/main/java/message/email/apimessageemail/CorsConfiguration.java
+++ src/main/java/message/email/apimessageemail/CorsConfiguration.java
@@ -3,11 +3,11 @@ package message.email.apimessageemail;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class CorsConfiguration extends WebMvcConfigurerAdapter {
+public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
